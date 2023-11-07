b7f7c716e7668cbd86689703a7d38208e00c56b0
cleung222@my.bcit.ca <cleung222@my.bcit.ca>
2023-11-06 21:23:01 -0800

    update components sidebar

--- .history/src/App_20231106211701.js
+++ .history/src/App_20231106211701.js
@@ -1,7 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import "./styles.css";
-
 
 function App() {
   return (
---
+++ src/components/Sidebar.js
@@ -0,0 +1,21 @@
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sideHeader">
+                <div className="logo">LOGO</div>
+                <div className="profile"></div>
+            </div>
+            <div className="sideCategory">
+                <div className="categoty select">Categoty</div>
+                <div className="categoty">Categoty</div>
+                <div className="categoty">Categoty</div>
+                <div className="categoty">Categoty</div>
+                <div className="categoty">Categoty</div>
+                <div className="categoty">Categoty</div>
+                <div className="categoty">Categoty</div>
+                <div className="categoty">Categoty</div>
+                <div className="categoty">Categoty</div>
+            </div>
+        </div>
+    );
+}
