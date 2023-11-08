b8106ef97437eaca7bce98d22752aaac748f60a9
jennydmcm <113193732+jennydmcm@users.noreply.github.com>
2023-11-07 23:21:29 -0800

    comments function added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Search from './components/Search/Search';
 import Post from './components/Post/Post';
+import Comment from './components/Comment/Comment'
 
 function App() {
   return (
     <div className="App">
       <Search />
       <Post />
+
     </div>
   );
 }
