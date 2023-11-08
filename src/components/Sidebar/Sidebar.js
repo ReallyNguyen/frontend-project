cd4cfacb8ee254cfe7abc034f4c73e4bf4c73617
cleung222@my.bcit.ca <cleung222@my.bcit.ca>
2023-11-07 20:34:44 -0800

    add sidebar module

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,23 @@
+import './Sidebar.css';
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sideHeader">
+                <div className="logo">LOGO</div>
+                <div className="profile"></div>
+            </div>
+            <div className="sideCategory">
+                <div className="category select">Categoty</div>
+                <div className="category">Category</div>
+                <div className="category">Category</div>
+                <div className="category">Category</div>
+                <div className="category">Category</div>
+                <div className="category">Category</div>
+                <div className="category">Category</div>
+                <div className="category">Category</div>
+                <div className="category">Category</div>
+            </div>
+        </div>
+    );
+}
