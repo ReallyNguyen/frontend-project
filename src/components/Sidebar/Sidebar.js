f35359c7072596f81de614185d62d40d3b194d03
cleung222@my.bcit.ca <cleung222@my.bcit.ca>
2023-11-14 16:44:48 -0800

    stylize sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,22 +1,23 @@
 import './Sidebar.css';
+import logo from './logo.svg';
+import profile from './profile.svg';
 
 export default function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sideHeader">
-                <div className="logo">LOGO</div>
-                <div className="profile"></div>
+                <img className="logo" src={logo} alt="" />
+                <img className="profile" src={profile} alt="" />
             </div>
             <div className="sideCategory">
-                <div className="category select">Categoty</div>
-                <div className="category">Category</div>
-                <div className="category">Category</div>
-                <div className="category">Category</div>
-                <div className="category">Category</div>
-                <div className="category">Category</div>
-                <div className="category">Category</div>
-                <div className="category">Category</div>
-                <div className="category">Category</div>
+                <div className="category select">Campus</div>
+                <div className="category">Student Life</div>
+                <div className="category">Study Group</div>
+                <div className="category">Housing</div>
+                <div className="category">Events</div>
+                <div className="category">Program</div>
+                <div className="category">Career</div>
+                <div className="category">Alumni</div>
             </div>
         </div>
     );
